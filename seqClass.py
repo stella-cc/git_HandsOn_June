e6b29defeae8796f102aaa44ff54cb8e06313be7
Stella <stella@MacBook-Pro-de-Stella.local>
2025-06-07 10:43:35 +0100

    comments in code

--- seqClass.py
+++ seqClass.py
@@ -4,10 +4,9 @@
 import sys, re
 from argparse import ArgumentParser
 
-#define arguments
+#arguments
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
-parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -15,23 +14,14 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-#capital letters
-args.seq = args.seq.upper()
+#convert input to uppercase
+args.seq = args.seq.upper()              
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):
+        print ('The sequence is DNA') # if cointains T is DNA
+    elif re.search('U', args.seq): # if contains U is RNA
         print ('The sequence is RNA')
-    else:
+    else: # if only contains A or G
         print ('The sequence can be DNA or RNA')
-else:
+else: # if contains any other letter than AGTCU
     print ('The sequence is not DNA nor RNA')
-
-#motifs
-if args.motif:
-    args.motif = args.motif.upper()
-    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
-    if re.search(args.motif, args.seq):
-        print("MOTIF FOUND in main!")
-    else:
-        print("MOTIF NOT FOUND!")
